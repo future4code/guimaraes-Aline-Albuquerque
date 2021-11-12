815de061fbeee904fceab89f712d2bf921351330
ALINE ALBUQUERQUE <line22ko@gmail.com>
2021-11-11 21:07:32 -0300

    exercícios-operadores

---
+++ modulo-1/operadores/template_javascript/index.js
@@ -0,0 +1,78 @@
+// const bool1 = true
+// const bool2 = false
+// const bool3 = !bool2
+
+// let resultado = bool1 && bool2
+// console.log("a. ", resultado) // Resultado: False pois, operador E (&&) retorna true se, e somente se, os todos booleanos envolvidos também forem true.
+
+// resultado = bool1 && bool2 && bool3 
+// console.log("b. ", resultado) // Reseultado False
+
+// resultado = !resultado && (bool1 || bool2) 
+// console.log("c. ", resultado) // Resultado True pois os valores comparados estão sendo true.
+
+// console.log("d. ", typeof resultado) // Booleano
+
+
+// Exercicio 2
+
+// Será impresso os números que o usuário digitar, porém não em forma de soma.
+
+
+// Exercício 3 
+
+// Ultilizando o conversor de string para número o resultado será somado. A sugestão de soma através do código seria desenvolvida da seguinte maneira:
+
+// let primeiroNumero = Number(prompt("Digite um numero!"))
+// let segundoNumero = Number(prompt("Digite outro numero!"))
+
+// const soma = primeiroNumero + segundoNumero
+
+// console.log(soma)
+
+
+//Exercio de escrita do código:
+
+// let idadeUsuario = Number(prompt("Qual é a sua idade?"))
+// let idadeDoAmigo = Number(prompt("Qual a idade do(a) seu(sua) melhor amigo(a)?"))
+// const ehMaior = idadeUsuario > idadeDoAmigo
+// const diferença = idadeUsuario - idadeDoAmigo
+// console.log("Sua idade é maior do que a do(da) seu(sua) melhor amigo(a)?", ehMaior)
+// console.log("A diferença de idade é:" , diferença)
+
+
+// Exercício 2 
+
+// let numeroPar = Number(prompt("Insira um número par"))
+// const restoDaDivisao = numeroPar % 2
+// console.log(restoDaDivisao)
+
+// O padrão é que o resto da divisão por ser um número par vai ser sempre igual a zero.
+// Se tentarmos dividir por um número ímpar, o padrão será sempre igual a um.
+
+
+// Exercício 3 
+
+// let idadeUsuario = Number(prompt("Qual sua idade em anos?"))
+// const idadeEmAnos = idadeUsuario * 12
+// const idadeEmDias = idadeUsuario * 365
+// const idadeEmhoras = idadeUsuario * 8766
+// console.log ("Sua idade em meses é:" , idadeEmAnos)
+// console.log("Sua idade em dias é:" , idadeEmDias)
+// console.log("Sua idade em horas é:" , idadeEmhoras)
+
+
+// Exercício 4
+
+// let primeiroNumero = Number(prompt("insira um número"))
+// let segundoNumero = Number(prompt("Insira outro número"))
+// const ehMaior = primeiroNumero > segundoNumero 
+// const ehIgual = primeiroNumero === segundoNumero
+// const ehDivisao = (primeiroNumero % segundoNumero) === 0
+// const ehDivisao2 = (segundoNumero % primeiroNumero) === 0
+// console.log("O primeiro numero é maior que segundo?" , ehMaior)
+// console.log("O primeiro numero é igual ao segundo? " , ehIgual)
+// console.log("O primeiro numero é divisível pelo segundo?" , ehDivisao)
+// console.log("O segundo numero é divisível pelo primeiro?" , ehDivisao2)
+
+
