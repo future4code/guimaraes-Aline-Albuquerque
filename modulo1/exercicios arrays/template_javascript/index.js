c8f6e41e372d68afb09f00e1130ea07059a0c2a1
ALINE ALBUQUERQUE <line22ko@gmail.com>
2021-11-17 21:12:59 -0300

    exercicios-arrays

---
+++ modulo1/exercicios arrays/template_javascript/index.js
@@ -0,0 +1,61 @@
+//EXERCICIO 1
+
+// let array
+// // console.log('a. ', array) // o array não tem valor definido, retorna underfined.
+
+// // array = null
+// // console.log('b. ', array) // o array retorna null que é o valor que foi atribuído.
+
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('c. ', array.length) // O length retorna o quantidade de intens dentro do array que é igual a 11.
+
+// let i = 0
+// console.log('d. ', array[i]) // O array retorna a posição de i que é = 0. O console responde 3, que é a posiçãpo de 0 no array.
+
+// array[i+1] = 19
+// console.log('e. ', array) // o array redifiniu a posição 1 com o valor de 19.
+
+// const valor = array[i+6]
+// console.log('f. ', valor) // o valor da constante "valor" é igual a 9.
+
+
+
+//EXERCICIO 2
+
+// const frase = prompt("Subi num onibus em marrocos")
+
+//  console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length) // o console retornará a frase em maíscula, substituindo todas as letras "A" pela letra "I" 
+// e contará o número de letras incluindo os espaços, retornando o valor total (no caso do exercicio = 27)).
+
+
+//EXERCICIO ESCRITA N1:
+
+//const nome = prompt("Qual é o seu nome?")
+// const email = prompt("Qual é seu email?")
+// console.log(`O email ${email} foi cadastrado com sucesso. Seja bem vinda(o),${nome}!`)//
+
+
+
+//EXERCICIO ESCRITA N2:
+
+// const array = ["queijo","pamonha","bolo de cenoura","bolinho de massa de pão","cheesecake"]
+// console.log(`Essas são minhas comidas preferidas:
+//  ${array[0]}
+//  ${array[1]}
+//  ${array[2]}
+//  ${array[3]}
+//  ${array[4]}`)
+
+
+// EXERCICIO ESCRITA N3:
+
+// const listaDeTarefas = []
+// const tarefas1 = prompt("Digite uma tarefa que você precise realizar no dia-a-dia")
+// const tarefas2 = prompt("Digite uma tarefa que você precise realizar no dia-a-dia")
+// const tarefas3 = prompt("Digite uma tarefa que você precise realizar no dia-a-dia")
+// listaDeTarefas.push(tarefas1,tarefas2,tarefas3)
+// console.log(listaDeTarefas)
+
+// const tarefasTodas = prompt("Qual dessas você já realizou? 0,1 ou 2?")
+// listaDeTarefas.splice(tarefasTodas,1)
+// console.log(listaDeTarefas)
